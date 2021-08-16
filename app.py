7640dc3a3accd211c622560c271ed4aaf53868ab
Bruno Cardoso <blimacardoso@fb.com>
2021-08-16 19:23:50 -0300

    hello world from service 1

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Serice 1"
+def root():
+    return "Service 1"
+
+@app.route("/hello/<username>")
+def hello(username):
+    return "Hello {} from Service 1".format(
+        'world' if username in None else username
+    )
